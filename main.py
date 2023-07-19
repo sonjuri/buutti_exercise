431b8bfe65fc5ba7ac47effa656e980023e4b796
sonjuri <sonja.rytinki@gmail.com>
2023-07-19 22:13:20 +0300

    added readme, changes to main.py

--- main.py
+++ main.py
@@ -1,35 +1,56 @@
 from collections import deque
 
+
 class MazeAlgorithm:
     def __init__(self, maze_file, max_moves):
         self.maze_file = maze_file
         self.max_moves = max_moves
         self.maze = self.read_maze_file()
 
-    ### Read txt file into 2D list
     def read_maze_file(self):
-        with open(self.maze_file, 'r') as file:
+        """
+        Read txt file into 2D list
+
+        :return: maze as 2D array
+        """
+        with open(self.maze_file, "r") as file:
             return [list(line.strip()) for line in file]
 
-    ### Get the size of the complete maze
     def get_maze_size(self):
-        maze_height = len(self.maze)     # Number of rows
-        maze_width = len(self.maze[0]) if self.maze else 0     # Number of columns if maze has at least one row
+        """
+        Get the size of the complete maze
+
+        :return: maze size as height and width
+        """
+        maze_height = len(self.maze)  # Number of rows
+        maze_width = (
+            len(self.maze[0]) if self.maze else 0
+        )  # Number of columns if maze has at least one row
         return maze_width, maze_height
 
-    ### Get Pentti's position in the maze
     def get_penttis_position(self, pentti):
-        maze_width, maze_height = self.get_maze_size()
-        y_temp = -1
+        """
+        Get Pentti's position in the maze
+
+        :pentti:
+        :return: x an y coordinates for Pentti's location
+        """
+        y = -1
         for line in self.maze:
-            y_temp += 1
+            y += 1
             if pentti in line:
                 x = line.index(pentti)
-        y = maze_height - y_temp
+                break
         return x, y
 
-    ### Move Pentti to different directions. Use the previous x and y values as current position.
     def move_pentti(self, position, direction):
+        """
+        Move Pentti to different directions. Use the previous x and y values as current position.
+
+        :position: Pentti's position in the maze
+        :direction: Direction of the movement
+        :return: New location for Pentti
+        """
         x, y = position
 
         if direction == "up":
@@ -41,33 +62,48 @@ class MazeAlgorithm:
         elif direction == "right":
             new_position = (x + 1, y)
         else:
-            raise ValueError("Pentti cannot go that way.")      # Very unlikely error to get in this case, but just to make sure
+            raise ValueError(
+                "Pentti cannot go that way."
+            )  # Very unlikely error to because breadth-first search is used, but just to make sure
 
         return new_position
 
-    ### Solve the given maze
-    ### Using deque to perform breadth-first search in order to find the optimal direction for each move
+
     def solve_maze(self):
+        """
+        Solve the given maze
+        Using deque to perform breadth-first search in order to find the optimal direction for each move
+
+        popleft() is used in the while-loop instead of pop() to ensure 'first in first out' in the queue.
+        Go through all of the possible directions, checks if the position is valid (is it's in the maze, it it's a wall, has it been visited)
+        
+        """
         start_position = self.get_penttis_position("^")
         queue = deque([(start_position, 0)])
         visited = set([start_position])
         maze_width, maze_height = self.get_maze_size()
 
         while queue:
-            current_position, moves = queue.popleft()    # popleft() used instead of pop() to ensure 'first in first out' in the queue
+            current_position, moves = queue.popleft()
+
+            if moves > self.max_moves:
+                print(f"Pentti could not find a way out from the maze after {self.max_moves} moves")
+                break
+
             x, y = current_position
 
             if self.maze[y][x] == "E":
-                print(f"Pentti escaped the maze in {moves} moves, good job Pentti. The maximum number of moves was {self.max_moves} and this was the {self.maze_file} maze.")
-                return True
+                print(
+                    f"Pentti escaped the maze in {moves} moves, good job Pentti."
+                    f"The maximum number of moves was {self.max_moves} and this was the {self.maze_file} maze."
+                )
+                break
 
-            ### Go through all of the possible directions
             directions = ["up", "down", "left", "right"]
             for direction in directions:
                 new_position = self.move_pentti(current_position, direction)
                 new_width, new_height = new_position
 
-                ### See if the new position is valid -> if it is in the maze, if it is a wall and if it has been visited before
                 if (
                     0 <= new_width < maze_width
                     and 0 <= new_height < maze_height
@@ -77,11 +113,8 @@ class MazeAlgorithm:
                     queue.append((new_position, moves + 1))
                     visited.add(new_position)
 
-        print(f"Pentti could not find a way out from the maze after {self.max_moves}")
-        return False
 
-
-### Tries for the first maze for each max moves
+"""Try the first maze for each set of max moves"""
 first_maze_file = "maze-task-first.txt"
 f_twenty = MazeAlgorithm(first_maze_file, max_moves=20)
 f_twenty.solve_maze()
@@ -92,7 +125,7 @@ f_hundred_fifty.solve_maze()
 f_two_hundred = MazeAlgorithm(first_maze_file, max_moves=200)
 f_two_hundred.solve_maze()
 
-### Tries for the second maze for each max moves
+"""Try the second maze for each set of max moves"""
 second_maze_file = "maze-task-second.txt"
 s_twenty = MazeAlgorithm(second_maze_file, max_moves=20)
 s_twenty.solve_maze()
---
+++ readme.md
@@ -0,0 +1,10 @@
+## Getting Pentti out of the maze
+- Built class that includes functions in order to get Pentti out of the maze in given moves (20, 150, 200)
+    - read_maze_file: Reads the given .txt file into a 2D array
+    - get_maze_size: Returns the size of the maze as the number of columns and rows
+    - get_penttis_position: Returns the position of Pentti in the maze
+    - move_pentti: Moves Pentti to different directions
+    - solve_maze: Solve the selected maze
+        - Uses breadth-first search
+
+- The code can only be ran locally in its current state
