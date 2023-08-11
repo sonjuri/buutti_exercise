337bb3336a60562beefa350325bd5be5823c3adb
sonjuri <sonja.rytinki@gmail.com>
2023-08-11 13:16:17 +0300

    changes to file usage and maze printing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import deque
 
 
@@ -6,6 +7,8 @@ class MazeAlgorithm:
         self.maze_file = maze_file
         self.max_moves = max_moves
         self.maze = self.read_maze_file()
+        self.start_position = None
+        self.exit_position = None
 
     def read_maze_file(self):
         """
@@ -13,8 +16,11 @@ class MazeAlgorithm:
 
         :return: maze as 2D array
         """
-        with open(self.maze_file, "r") as file:
-            return [list(line.strip()) for line in file]
+        try:
+            with open(self.maze_file, "r") as file:
+                return [list(line.strip()) for line in file]
+        except FileNotFoundError:
+            print(f"File '{self.maze_file}' was not found")
 
     def get_maze_size(self):
         """
@@ -28,18 +34,17 @@ class MazeAlgorithm:
         )  # Number of columns if maze has at least one row
         return maze_width, maze_height
 
-    def get_penttis_position(self, pentti):
+    def get_penttis_position(self):
         """
         Get Pentti's position in the maze
-
-        :pentti:
+        
         :return: x an y coordinates for Pentti's location
         """
         y = -1
         for line in self.maze:
             y += 1
-            if pentti in line:
-                x = line.index(pentti)
+            if "^" in line:
+                x = line.index("^")
                 break
         return x, y
 
@@ -68,20 +73,22 @@ class MazeAlgorithm:
 
         return new_position
 
-
     def solve_maze(self):
         """
         Solve the given maze
         Using deque to perform breadth-first search in order to find the optimal direction for each move
-
+        The positions that are present in the code are stored in position_dict
         popleft() is used in the while-loop instead of pop() to ensure 'first in first out' in the queue.
         Go through all of the possible directions, checks if the position is valid (is it's in the maze, it it's a wall, has it been visited)
         
+        :return: dictionary of the positions in the maze
         """
-        start_position = self.get_penttis_position("^")
-        queue = deque([(start_position, 0)])
-        visited = set([start_position])
+        self.start_position = self.get_penttis_position()
+        queue = deque([(self.start_position, 0)])
+        visited = set([self.start_position])
         maze_width, maze_height = self.get_maze_size()
+        position_dict = {}
+        solved = False
 
         while queue:
             current_position, moves = queue.popleft()
@@ -97,7 +104,10 @@ class MazeAlgorithm:
                     f"Pentti escaped the maze in {moves} moves, good job Pentti."
                     f"The maximum number of moves was {self.max_moves} and this was the {self.maze_file} maze."
                 )
-                break
+                solved = True
+                self.exit_position = current_position
+                self.mark_path(position_dict)
+                return position_dict, solved
 
             directions = ["up", "down", "left", "right"]
             for direction in directions:
@@ -112,26 +122,46 @@ class MazeAlgorithm:
                 ):
                     queue.append((new_position, moves + 1))
                     visited.add(new_position)
+                    position_dict[new_position] = current_position
+
+        return position_dict, solved
 
 
-"""Try the first maze for each set of max moves"""
-first_maze_file = "maze-task-first.txt"
-f_twenty = MazeAlgorithm(first_maze_file, max_moves=20)
-f_twenty.solve_maze()
+    def mark_path(self, position_dict):
+        """
+        Mark the optimal path on the maze
 
-f_hundred_fifty = MazeAlgorithm(first_maze_file, max_moves=150)
-f_hundred_fifty.solve_maze()
+        :position_dict: Dictionary where the positions are saved to
+        """
+        path_position = self.exit_position
+        while path_position != self.start_position:
+            x, y = path_position
+            if path_position not in position_dict:
+                break
+            self.maze[y][x] = "*"
+            path_position = position_dict[path_position]
+            
 
-f_two_hundred = MazeAlgorithm(first_maze_file, max_moves=200)
-f_two_hundred.solve_maze()
+    def print_maze(self):
+        """
+        Print the maze with the optimal solution(s)
+        """
+        for row in self.maze:
+            print(''.join(row))
 
-"""Try the second maze for each set of max moves"""
-second_maze_file = "maze-task-second.txt"
-s_twenty = MazeAlgorithm(second_maze_file, max_moves=20)
-s_twenty.solve_maze()
+parser = argparse.ArgumentParser()
+parser.add_argument('maze_file', type=str,
+                    help='The maze file to be solved')
+parser.add_argument('max_moves', type=int,
+                    help='Max moves to be used to solve the maze')
+args = parser.parse_args()
 
-s_hundred_fifty = MazeAlgorithm(second_maze_file, max_moves=150)
-s_hundred_fifty.solve_maze()
+maze_file = args.maze_file
+max_moves = args.max_moves
+maze_alg = MazeAlgorithm(maze_file, max_moves=max_moves)
 
-s_two_hundred = MazeAlgorithm(second_maze_file, max_moves=200)
-s_two_hundred.solve_maze()
+if maze_alg.maze:
+    position_dict_first_maze, solved = maze_alg.solve_maze()
+    if solved:
+        maze_alg.mark_path(position_dict_first_maze)
+        maze_alg.print_maze()
